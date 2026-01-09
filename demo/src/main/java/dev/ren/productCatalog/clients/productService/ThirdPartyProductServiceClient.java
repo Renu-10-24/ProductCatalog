cf7692653a4b017fd2903b5c80cb2ded8902d325
Renu6 <Renu.rachakulla@gmail.com>
2026-01-09 01:19:58 -0600

    For now, not implementing this interface, coded directly the FakeStoreProductServiceClient class which is its implementation

--- demo/src/main/java/dev/ren/productCatalog/clients/productService/ThirdPartyProductServiceClient.java
+++ demo/src/main/java/dev/ren/productCatalog/clients/productService/ThirdPartyProductServiceClient.java
@@ -4,7 +4,7 @@ import dev.ren.productCatalog.dtos.FakeStoreProductDTO;
 import dev.ren.productCatalog.dtos.GenericProductDTO;
 import dev.ren.productCatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
-
+//not used currently to avoid complexity, if we are planning to migrate to different third party APIs, we can go for this client interface implementation
 public interface ThirdPartyProductServiceClient {
     FakeStoreProductDTO getProductById(long id);
 
